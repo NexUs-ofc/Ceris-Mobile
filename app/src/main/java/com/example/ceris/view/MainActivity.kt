1e976109fe27d75e31092e49a0f5c3d6d6d81c1e
RicardoCiriloJunior <ricardocjr07@gmail.com>
2026-07-13 20:08:25 -0300

    build: create a simple mvvm structure

--- app/src/main/java/com/example/ceris/view/MainActivity.kt
+++ app/src/main/java/com/example/ceris/view/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.example.ceris
+package com.example.ceris.view
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.ceris.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
