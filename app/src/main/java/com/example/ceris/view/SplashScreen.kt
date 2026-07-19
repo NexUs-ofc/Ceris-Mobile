12260b8a0f3e80fcb42bf8e2270fb8fa62c5b2af
Eduardo_Farias_cyber <eduradofarias123do@gmail.com>
2026-07-19 17:24:23 -0300

    feat: Adição de Splash Screen básica

---
+++ app/src/main/java/com/example/ceris/view/SplashScreen.kt
@@ -0,0 +1,29 @@
+package com.example.ceris.view
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.ceris.R
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash_screen)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        Thread{
+            Thread.sleep(3000)
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }.start()
+    }
+}
